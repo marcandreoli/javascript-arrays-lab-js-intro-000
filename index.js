65b83514e6792e4a936e64d597baf27fa326f3de
marcantoineandreoli@gmail.com Andreoli <marcantoineandreoli@gmail.com>
2018-03-20 21:25:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much.";
 
-function destructivelyAppendKitten(name) {
+function destructivelyAppendKitten(kittens, name) {
   var newArray = [...kittens, name];
   return newArray;
 };
